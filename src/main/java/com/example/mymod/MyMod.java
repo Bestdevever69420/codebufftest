01d1448343e63c71de8b82872de59a9292612689
Bestdevever69420 <eddy06532@gmail.com>
2026-02-22 06:52:51 +1000

    chore: Update version to 1.1.0 and add AoA3 dependency

--- src/main/java/com/example/mymod/MyMod.java
+++ src/main/java/com/example/mymod/MyMod.java
@@ -36,7 +36,7 @@ import net.minecraftforge.common.BiomeManager;
 public class MyMod {
     public static final String MODID = "mymod";
     public static final String NAME = "My Mod";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     @Mod.Instance(MODID)
     public static MyMod instance;
